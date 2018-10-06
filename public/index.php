0fc0fc85410ec92dbfcdbfce903874b36ffe3cc8
hema <hemamounikakotte@gmail.com>
2018-10-05 21:32:56 -0400

    Read CSV file

--- public/index.php
+++ public/index.php
@@ -5,16 +5,31 @@
  * Date: 10/4/18
  * Time: 9:47 PM
  */
-main::start();
+main::start(example.csv);
 
 class main  {
 
-    static public function start() {
+    static public function start($filename) {
+
+
+$records = csv::getrecords(filename);
+print_r($records);
+    }
+}
+
+class csv {
+    static public function getrecords($filename) {
 
         $file = fopen("example.csv","r");
-        print_r(fgetcsv($file));
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            $records[] = $record;
+        }
+
         fclose($file);
-        
+        print_r($records);
     }
 }
 
