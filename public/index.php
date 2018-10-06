36144837de274036353074a3c6b741faa82eeb11
hema <hemamounikakotte@gmail.com>
2018-10-05 21:05:08 -0400

    Test: edit index file

--- public/index.php
+++ public/index.php
@@ -5,5 +5,18 @@
  * Date: 10/4/18
  * Time: 9:47 PM
  */
+main::start();
 
-echo 'test1234hello1234567';
+class main  {
+
+    static public function start() {
+
+        $file = fopen("example.csv","r");
+        print_r(fgetcsv($file));
+        fclose($file);
+        
+    }
+}
+
+
+
